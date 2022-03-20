3f4a6a4456abae2ed27378228286c2a2a57a7b0c
freddycansic <freddycansick@gmail.com>
2022-03-20 20:54:17 +0000

    Added GLEW library

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -1,44 +1,59 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <cmath>
+#include <iostream>
+
+void errorCallback(int error, const char* description)
+{
+	std::cerr << "Error: " << description << "";
+}
 
 int main(void)
 {
 	GLFWwindow* window;
 
-	/* Initialize the library */
+	// initialise GLFW
 	if (!glfwInit())
 		return -1;
 
-	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-	if (!window)
-	{
+	// get vidmode of primary monitor
+	const GLFWvidmode* vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+	// create window + context, set to 3/4 screen width + height
+	window = glfwCreateWindow(3 * vidmode->width / 4, 3 * vidmode->height / 4, "Hello World", NULL, NULL);
+	if (!window) {
 		glfwTerminate();
 		return -1;
 	}
 
-	/* Make the window's context current */
+	// callbacks
+	glfwSetErrorCallback(errorCallback);
+
+	// set opengl rendering context
 	glfwMakeContextCurrent(window);
 
-	/* Loop until the user closes the window */
-	while (!glfwWindowShouldClose(window))
-	{
-		/* Render here */
+	// initialise GLEW, must be called after there is a opengl rendering context
+	if (glewInit() != GLEW_OK)
+		return -1;
+
+	while (!glfwWindowShouldClose(window)) {
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		// triangle
 		glBegin(GL_TRIANGLES);
 		glVertex2f(-0.5f, -0.5f);
-		glVertex2f(0.0f, sqrt(3)/2);
+		glVertex2f(0.0f, (float) sqrt(3) / 2);
 		glVertex2f(0.5f, -0.5f);
 		glEnd();
 
-		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
 
-		/* Poll for and process events */
 		glfwPollEvents();
 	}
 
+	// destroy windows and release resources used by GLFW
+	glfwDestroyWindow(window);
 	glfwTerminate();
+
 	return 0;
 }
