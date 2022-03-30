b9ce745d0a8ec9a9e78cead65feedd1a571aacc3
freddycansic <freddycansick@gmail.com>
2022-03-30 16:16:28 +0100

    Union Vec3

--- OpenGL/src/maths/Vectors.h
+++ OpenGL/src/maths/Vectors.h
@@ -1,14 +1,13 @@
 #pragma once
 
 template<typename T>
-struct Vec3 {
-
-	T x, y, z;
-
+union Vec3 {
+	struct {T x, y, z;};
+	struct {T r, g, b;};
 };
 
 template<typename T>
 struct Vec2 {
 	T x, y;
-
 };
+
