2017c94beda7dffa6a2d4b9a1e7935725f391bde
freddycansic <freddycansick@gmail.com>
2022-04-25 20:35:24 +0100

    Weird transformations

--- OpenGL/src/engine/rendering/shapes/Cube.cpp
+++ OpenGL/src/engine/rendering/shapes/Cube.cpp
@@ -48,19 +48,19 @@ Cube::Cube(float x, float y, float z, float size)
 
 void Cube::recalculateVertices()
 {
-	Mat4 transformMatrix = Mat4::identity.translate(m_Transform.tra.x, m_Transform.tra.y, m_Transform.tra.z).rotate(m_Transform.rot.x, m_Transform.rot.y, m_Transform.rot.z).scale(m_Transform.sca.x, m_Transform.sca.y, m_Transform.sca.z);
-
-	std::cout << transformMatrix << std::endl;
+	//Mat4 transformMatrix = Mat4::identity.translate(m_Transform.tra.x, m_Transform.tra.y, m_Transform.tra.z).rotate(m_Transform.rot.x, m_Transform.rot.y, m_Transform.rot.z).scale(m_Transform.sca.x, m_Transform.sca.y, m_Transform.sca.z);
 
 	for (unsigned int i = 0; i < s_UnitVertices.size(); i++) {
 		const auto& unitPos = s_UnitVertices[i].position; // unit pos
-		auto& resultPos = m_Vertices[i].position; // result vertex
+		auto& resultPos = m_Vertices[i].position; // result pos
 		
-		std::cout << "UNIT\n" << Vec4(unitPos, 1.0f) << std::endl;
-		// apply transformation to unit vertex and store in result vertex
-		resultPos = Vec3(Vec4(unitPos, 1.0f) * transformMatrix);
+		std::cout << "UNIT\n" << unitPos << std::endl;
 
-		std::cout << "END\n" << Vec4(resultPos, 1.0f) << std::endl;
+		// apply transformation to unit pos and store in result pos
+		//resultPos = Vec3(Vec4(unitPos, 1.0f) * transformMatrix);
+		// TODO could change it from identity.scale to Mat4::scale
+		resultPos = Vec3(Vec4(unitPos, 1.0f) * Mat4::identity.scale(m_Transform.sca.x, m_Transform.sca.y, m_Transform.sca.z) * Mat4::identity.rotate(m_Transform.rot.x, m_Transform.rot.y, m_Transform.rot.z) * Mat4::identity.translate(m_Transform.tra.x, m_Transform.tra.y, m_Transform.tra.z));
+		std::cout << "RESULT\n" << resultPos << std::endl;
 	}
 }
 
