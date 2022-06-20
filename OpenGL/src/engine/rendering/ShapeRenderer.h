528b1fac70993e970081f67345fcb87963155abb
freddycansic <freddycansick@gmail.com>
2022-06-20 12:27:11 +0100

    Refactored ShapeRenderer class to a namespace

--- OpenGL/src/engine/rendering/ShapeRenderer.h
+++ OpenGL/src/engine/rendering/ShapeRenderer.h
@@ -1,67 +1,22 @@
 #pragma once
 
-#include <memory>
+class Texture;
+class Shape;
+struct Vec4;
 
-#include "engine/rendering/opengl/Texture.h"
-#include "engine/rendering/opengl/VertexArray.h"
-#include "engine/rendering/opengl/VertexBuffer.h"
-#include "engine/rendering/opengl/IndexBuffer.h"
-#include "engine/rendering/opengl/Shader.h"
-#include "engine/rendering/Vertex.h"
-
-#include "shapes/Shape.h"
-
-struct Batch {
-	std::vector<Vertex> vertices;
-	std::vector<unsigned int> indices;
-	uint64_t texHandle;
-};
-
-class ShapeRenderer
+namespace ShapeRenderer
 {
-
-private:
-	enum class State {
-		UNINITIALISED,
-		STOPPED,
-		BEGUN,
-	};
-
-	// little bit scared these magic numbers will come back to bite me
-	constexpr static size_t MAX_VERTICES = 12000000;
-	constexpr static size_t MAX_INDICES = 20000000; // <-- INDICES LIMIT LIMITS TO ~1728 cubes
-	constexpr static size_t MAX_TEXTURES = 1024;
-	
-	static State state;
-
-	// vertex + index data
-	static std::vector<Batch> s_Batches;
-
-	static std::unique_ptr<VertexArray> s_Vao;
-	static std::unique_ptr<VertexBuffer> s_Vbo;
-	static std::unique_ptr<IndexBuffer> s_Ibo;
-	static std::unique_ptr<Shader> s_Shader;
-
-	static unsigned int maxIndex;
-	static unsigned int lastMaxShapeIndex;
-
-	static void addShapeIndices(std::vector<unsigned int>& indexBuffer, const Shape& shape);
-	static void checkBatchReady();
-
-public:
-	ShapeRenderer() = delete;
-	~ShapeRenderer() = delete;
-
 	/**
 	 * Initialises batch rendering, must be called once before the first begin() call.
 	 * @brief Initialises batch rendering.
-	*/	static void init();
+	**/
+	void init();
 
 	/**
 	 * Start batch rendering. Must be called after init() has been called once.
 	 * @brief Starts batch rendering.
 	**/
-	static void begin();
+	void begin();
 
 	/**
 	 * Draw a shape using a solid color.
@@ -69,7 +24,7 @@ public:
 	 * @param shape Shape to use.
 	 * @param color Color to use as a Vec4.
 	**/
-	static void draw(Shape& shape, const Vec4& color);
+	void draw(Shape& shape, const Vec4& color);
 
 	/**
 	 * Draw a shape using a texture.
@@ -77,12 +32,12 @@ public:
 	 * @param shape Shape to use.
 	 * @param tex Texture to use.
 	**/
-	static void draw(Shape& shape, const Texture& tex);
+	void draw(Shape& shape, const Texture& tex);
 
 	/**
 	 * Ends the batch and draws to the screen.
 	 * @brief Ends the batch.
 	*/
-	static void end();
+	void end();
 };
 
