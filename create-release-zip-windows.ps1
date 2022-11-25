987f1bd5665c082cfe18f822b4d25d75d60b5c4f
freddycansic <freddycansick@gmail.com>
2022-11-25 11:27:37 +0000

    .

--- create-release-zip-windows.ps1
+++ create-release-zip-windows.ps1
@@ -1,21 +1,22 @@
-$currentPath = Get-Location
-function Get-Absolute-Path
+function Get-Path
 {
     param 
     (
         [string] $relativePath
     )
 
-
-    return Join-Path $currentPath $relativePath
+    return Join-Path $PSScriptRoot $relativePath
 }
 
 # build solution
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe OpenGL.sln /property:Configuration=Release | Out-Null # makes powershell wait for process to finish before moving on
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe OpenGL/OpenGL.vcxproj /property:Configuration=Release | Out-Null # makes powershell wait for process to finish before moving on
 
 # copy res folder into build path
-Copy-Item (Get-Absolute-Path "OpenGL\res") -Destination (Get-Absolute-Path "bin\x64Release") -Force -Recurse
+Copy-Item (Get-Path "OpenGL\res") -Destination (Get-Path "bin\x64Release") -Force -Recurse
+
+# Write-Output $currentPath
+# Write-Output (Get-Absolute-Path "OpenGL\res")
 
 $zipName = Read-Host -Prompt "Enter zip name e.g v0.1.0-alpha"
 
-Compress-Archive -LiteralPath ((Get-Absolute-Path "bin\x64Release\OpenGL.exe"),(Get-Absolute-Path "bin\x64Release\res")) -DestinationPath (Get-Absolute-Path "\bin\x64Release\$zipName.zip")
+Compress-Archive -LiteralPath ((Get-Path "bin\x64Release\OpenGL.exe"),(Get-Path "bin\x64Release\res")) -DestinationPath (Get-Path "\bin\x64Release\$zipName.zip")
